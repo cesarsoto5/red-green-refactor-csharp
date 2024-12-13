25d16fdf87fb25d71b1e62bcebe5e8bf193abc3c
Cesar Soto <cesar.sotoc@outlook.com>
2024-12-13 00:43:50 -0600

    Add files via upload

---
+++ FactorialService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Example
+{
+    public class FactorialService
+    {
+        public int Calculate(int number)
+        {
+            if (number == 0) 
+                return 1;       // El factorial de 0 es 1.
+
+            int result = 1;
+            for (int index = 1; index <= number; index++)
+            {
+                result *= index; // Multiplica el resultado acumulado por el índice actual.
+            }
+            return result;      // Devuelve el resultado del factorial.
+        }    
+    }
+}
---
+++ FactorialServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Testing.Example.Test
+{
+    public class FactorialServiceTests
+    {
+        [Fact]
+        public void TestFactorialZero()
+        {
+            //Arrange
+            FactorialService service = new FactorialService();
+            
+            //Act
+            int result = service.Calculate(0);
+
+            //Assert
+            Assert.Equal(1, result);
+        }
+        
+        [Fact]
+        public void TestFactorialFive()
+        {
+            FactorialService service = new FactorialService();
+
+            int result = service.Calculate(5);
+
+            Assert.Equal(120, result);
+        }
+    }
+}
